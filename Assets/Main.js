3fa7c412e7fb3a8c85d00786e058643c1d8ad2d6
RonnerBBQ <Ronner@live.se>
2015-04-09 17:56:08 +0200

    Test commit

---
+++ Assets/Main.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var clicks : Transform;
+var score : GUIText;
+var clicked : boolean = false;
+var click : int = 0;
+
+function Start () 
+{
+
+}
+
+function Update () 
+{
+	score.text = "Clicks: " + click;
+}
+
+function OnMouseDown ()
+{
+	click += 1;
+}
