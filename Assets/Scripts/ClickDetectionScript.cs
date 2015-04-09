bcd083c9b9b0599700dcef86ae7b7f2b93418fe6
DealerUmbra <gaharion@gmail.com>
2015-04-09 22:31:21 +0200

    4
    ...Borde fungera?

---
+++ Assets/Scripts/ClickDetectionScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickDetectionScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		print ("Detects keydown");
+		GameObject.Find ("ScoreText").GetComponent<ScoreTextScript> ().gainPoint ();
+	}
+}
